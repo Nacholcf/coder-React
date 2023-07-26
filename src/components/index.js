248169caca6eeee9ecd2c1e75a42bbe9a47b2f28
nacholcf <ignaciomarquezcf@gmail.com>
2023-07-26 19:05:24 -0300

    19.05

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { NavBar } from './NavBar/NavBar'
+export { NavBar } from './NavBar/NavBar';
 export { Item } from './Item/Item';
 export { ItemCount } from "./ItemCount/Itemcount";
 export {ItemListContainer } from './ItemListContainer/ItemListContainer';
